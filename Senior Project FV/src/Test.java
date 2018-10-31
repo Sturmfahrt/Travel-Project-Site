7a8a77825bc080271e09e686b828b6bd023294d0
Ethan <32519710+Magikro@users.noreply.github.com>
2018-10-31 15:37:17 -0400

    cleaned up stuff and documented

--- Senior Project FV/src/Test.java
+++ Senior Project FV/src/Test.java
@@ -15,10 +15,12 @@ public abstract class Test {
 		//cities(1);
 		
 		Document doc = Jsoup.connect("https://weather.com/weather/tenday/l/Davie+FL+USFL0577:1:US").get(); //highest number for Florida is 1201
-		System.out.println(doc.title()+":");
+		//connects to the web page
+		
 		Elements temp = doc.select(".temp span");
 		Elements date = doc.select(".twc-sticky-col span");
 		Elements precip = doc.select(".precip span span");
+		//gets certain parts of the document and assigns them to elements
 	
 		
 		String text = temp.text();
@@ -29,14 +31,15 @@ public abstract class Test {
 		String[] curry = text.split(" ");
 		String[] rice = dative.split(" ");
 		String[] soup = rain.split(" ");
+		//all of this changes the info it gathers to a string and then that string into an array
 		
 		String area = fried[0]+ " "+fried[1];
-		
-		System.out.println(rain);
+		//just creates the name of the area you searched into a string
 		
 		int adder = 0;
 		int saddler = 0;
 		int padd = 0;
+		//initializing the integers
 		System.out.println("The forecast for "+area+": ");
 		try {
 			for(int i = 0; curry.length > i; i++) {
@@ -53,7 +56,7 @@ public abstract class Test {
 		System.out.println("Things work!");
 	}
 	
-	public static void cities(int l) throws IOException {
+	public static void cities(int l) throws IOException { //this will get all the city names and put them into an array
 		try {
 		while(l != 1202) {
 			Document docum = Jsoup.connect("https://weather.com/weather/tenday/l/Davie+FL+USFL"+String.format("%04d", l)+":1:US").get();
@@ -72,7 +75,7 @@ public abstract class Test {
 
 	}
 	
-	public static void writer(String popcorn) throws IOException {
+	public static void writer(String popcorn) throws IOException { //writes stuff into a text file
 		
 		String FILENAME = "test\\cities.txt";
 		
