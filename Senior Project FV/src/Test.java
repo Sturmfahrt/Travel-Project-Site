bc66d86b03440cf818c7e58f5550c89a580021c9
Ethan <32519710+Magikro@users.noreply.github.com>
2018-10-31 15:26:32 -0400

    added precipitation and starting on areas

--- Senior Project FV/src/Test.java
+++ Senior Project FV/src/Test.java
@@ -12,32 +12,39 @@ public abstract class Test {
 
 	public static void main(String[] args) throws IOException {
 		
-		cities(1);
+		//cities(1);
 		
 		Document doc = Jsoup.connect("https://weather.com/weather/tenday/l/Davie+FL+USFL0577:1:US").get(); //highest number for Florida is 1201
 		System.out.println(doc.title()+":");
 		Elements temp = doc.select(".temp span");
 		Elements date = doc.select(".twc-sticky-col span");
+		Elements precip = doc.select(".precip span span");
 	
 		
 		String text = temp.text();
 		String name = doc.title();
 		String dative = date.text();
+		String rain = precip.text();
 		String[] fried = name.split(" ");
 		String[] curry = text.split(" ");
 		String[] rice = dative.split(" ");
+		String[] soup = rain.split(" ");
 		
 		String area = fried[0]+ " "+fried[1];
 		
+		System.out.println(rain);
+		
 		int adder = 0;
 		int saddler = 0;
+		int padd = 0;
 		System.out.println("The forecast for "+area+": ");
 		try {
 			for(int i = 0; curry.length > i; i++) {
 				
-				System.out.println("Temparature for "+rice[adder]+", "+rice[adder + 1]+" "+rice[adder + 2]+" is High: "+curry[saddler]+" / Low: "+curry[saddler + 2]);
+				System.out.println("Temparature for "+rice[adder]+", "+rice[adder + 1]+" "+rice[adder + 2]+" is High: "+curry[saddler]+" / Low: "+curry[saddler + 2]+" and the chance of precipitation is "+soup[padd]);
 				adder += 3;
 				saddler += 3;
+				padd += 2;
 			}
 		} catch(ArrayIndexOutOfBoundsException e) {
 			
@@ -54,7 +61,7 @@ public abstract class Test {
 			String[] citiName = citi.split(",");
 			String cities = citiName[0];
 			System.out.println(cities + ": "+String.format("%04d", l));
-			writer(cities+": "+String.format("%04d", l));
+			//writer(cities+": "+String.format("%04d", l));
 			l++;
 			
 		}
