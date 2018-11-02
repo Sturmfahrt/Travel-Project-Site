eeb049e8973c82e03a5423d597274cabf4438b87
Max <sirmaxalot305@gmail.com>
2018-11-02 15:27:56 -0400

    fixed the icon on tab for loginpage

--- login.php
+++ login.php
@@ -4,7 +4,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="icon" href="https://getbootstrap.com/favicon.ico">
+    <link rel="icon" href="vapor.png">
 
     <title>Signin</title>
 
