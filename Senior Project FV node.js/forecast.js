ddbfbb5f688ec776444ef0de212b60b5f3873dc9
Max <sirmaxalot305@gmail.com>
2018-11-09 14:52:23 -0500

    forecast node port almost done.
    alright well the node port of forecast.java is almost complete. all thats left is to figure out how to get the chance of rain.

--- Senior Project FV node.js/forecast.js
+++ Senior Project FV node.js/forecast.js
@@ -1,17 +1,17 @@
 //this is the node.js port of the java forcast program.
 
-let noodle = require('noodlejs');
-var query = {
-    url: 'http://google.com/search?q=javascript',
-    type: 'html',
-    selector: 'h3.r a',
-    extract: 'text'
-  },
-  uriQuery = encodeURIComponent(JSON.stringify(query)),
-  request  = 'http://example.noodlejs.com/?q=' +
-             uriQuery + '&callback=?';
+const request = require('superagent');
 
-// Make Ajax request to Noodle server
-jQuery.getJSON(request, function (data) {
-  alert(data[0].results);
-});
+request
+  .get('http://api.openweathermap.org/data/2.5/weather?q=Miami&APPID=8ed55356b0c310798b7216c27917e611')
+  .end((err, res) => {
+    if(err){console.print(err)}
+    else {
+      console.log("Weather in " + res.body.name + " is: " + res.body.weather[0].description );
+      console.log("Temperature in " + res.body.name + " is: " + keltofar(res.body.main.temp) + " Degrees" );
+    }
+  });
+
+function keltofar(kelvin) {
+  return Math.floor(kelvin * 9/5 - 459.67);
+}
