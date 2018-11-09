4b9b110ca0d6eb7fb7ae6c7cb8328d2e8c3be9dc
Max <sirmaxalot305@gmail.com>
2018-11-09 16:11:48 -0500

    tried to get forecast.js to accept parameters
    tried to get forecast.js to accept parameters but i borked it up.

--- Senior Project FV node.js/forecast.js
+++ Senior Project FV node.js/forecast.js
@@ -2,10 +2,13 @@
 
 const request = require('superagent');
 
+var urlToPass = 'http://api.openweathermap.org/data/2.5/weather?q=' + inputName + '&APPID=8ed55356b0c310798b7216c27917e611';
+var inputName = process.argv[2];
+
 request
-  .get('http://api.openweathermap.org/data/2.5/weather?q=Miami&APPID=8ed55356b0c310798b7216c27917e611')
+  .get(urlToPass) /* hopefully this allows it to use input for the city name */
   .end((err, res) => {
-    if(err){console.print(err)}
+    if(err){console.log(err)}
     else {
       console.log("Weather in " + res.body.name + " is: " + res.body.weather[0].description );
       console.log("Temperature in " + res.body.name + " is: " + keltofar(res.body.main.temp) + " Degrees" );
