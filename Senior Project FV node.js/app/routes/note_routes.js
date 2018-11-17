5171320e0b5dad7bccb67ab82b259e90595b0c51
Sirmaxalot305 <Sirmaxslot305@icloud.com>
2018-11-16 20:13:31 -0500

    started work on the foundation of the api
    started work on the foundation of the api. but we need to get git to ignore the node_modules folder.

---
+++ Senior Project FV node.js/app/routes/note_routes.js
@@ -0,0 +1,8 @@
+// routes/note_routes.js
+
+module.exports = function(app, db) {
+  app.post('/notes', (req, res) => {
+    console.log(req.body)
+    res.send('Hello')
+  });
+};
---
+++ Senior Project FV node.js/server.js
@@ -0,0 +1,17 @@
+// this is going to be the core of the api.
+// server.js
+
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const port = 8000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+require('./app/routes')(app, {});
+app.listen(port, () => {
+  console.log('We are live on ' + port);
+});
