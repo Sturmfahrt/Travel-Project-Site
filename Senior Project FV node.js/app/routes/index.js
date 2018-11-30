33d277be86f3ddd5075cb1bd678510bdd7042128
Ethan <32519710+Magikro@users.noreply.github.com>
2018-11-30 13:59:04 -0500

    did what max told me to do

--- Senior Project FV node.js/app/routes/index.js
+++
@@ -1,8 +0,0 @@
-// routes/index.js
-
-const noteRoutes = require('./note_routes');
-
-module.exports = function(app, db) {
-  noteRoutes(app, db);
-  // Other route groups could go here, in the future
-}
--- Senior Project FV node.js/app/routes/note_routes.js
+++
@@ -1,8 +0,0 @@
-// routes/note_routes.js
-
-module.exports = function(app, db) {
-  app.post('/notes', (req, res) => {
-    console.log(req.body)
-    res.send('Hello')
-  });
-};
