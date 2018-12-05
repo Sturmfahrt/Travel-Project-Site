85663e0189ae512a6d106b9d70887afe4653dc9f
CyberHead <Sirmaxslot305@gmail.com>
2018-12-05 16:04:42 -0500

    attempted more fixing.
    close to working. but the return statement seems to be using the delorean instead of the timeout.

--- Senior Project FV node.js/api/controllers/weatherController.js
+++ Senior Project FV node.js/api/controllers/weatherController.js
@@ -1,15 +1,10 @@
-var weatherData = require('../../weather/forecast');
+var getWeatherData = require('../../weather/forecast');
 var inputName;
 
 exports.read_weather = function(req, res) {
-    module.exports.inputName = req.params.cityName;
-    console.log("weatherData promise is about to be called");
-    weatherData.then( (message) => {
-        console.log("weatherData promise has been called.");
-        res.send(message);
-    })
-    .catch(function(error){
-        console.log("weatherData promise has been called. catch statement");
-        console.error(error);
-      });
+    inputName = req.query.cityName;
+    console.log("inputName before getWeatherData is called: " + inputName);
+    var data = getWeatherData(inputName);
+    console.log("data controller received is: " + data);
+    res.send(data);
 };
--- Senior Project FV node.js/weather/forecast.js
+++ Senior Project FV node.js/weather/forecast.js
@@ -2,13 +2,17 @@
 
 const request = require('superagent');
 
-var inputName = require('../api/controllers/weatherController');
-var urlToPass = "http://api.openweathermap.org/data/2.5/weather?q=" + inputName + "&APPID=8ed55356b0c310798b7216c27917e611";
+var inputName;
+var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + inputName + "&APPID=8ed55356b0c310798b7216c27917e611";
 var weatherData = "weatherData test value";
 var requestError = "requestError test value";
+var returnVal = "currently fucking broken";
+var dataRetreivedSuccessfully = false;
+var timerDone;
 //console.log(inputName);
 //console.log(urlToPass);
 
+/*
 var getWeather = new Promise(async (resolve, reject) => {
   let dataRetreivedSuccessfully = false;
   
@@ -35,8 +39,53 @@ var getWeather = new Promise(async (resolve, reject) => {
     reject(requestError);
     console.log("rejected");
   }
-});
-module.exports = getWeather;
+}); */
+module.exports = function(cityName) {
+  timerDone = false;
+  setCityName(cityName);
+  console.log("returnVal: " + returnVal);
+  console.log("returnVal has yet to be changed.");
+  setTimeout(returnValSetter, 1000);
+  console.log("returnVal should have been changed");
+  console.log("returnVal: " + returnVal);
+  return returnVal;
+};
+function setCityName(cityName) {
+  console.log("cityName: " + cityName);
+  inputName = cityName;
+  console.log("inputName: " + inputName);
+  apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + inputName + "&APPID=8ed55356b0c310798b7216c27917e611";
+  weatherDataRequester(apiUrl);
+};
+function weatherDataRequester(urlToPass) {
+  console.log("urlToPass: " + urlToPass);
+  request
+  .get(urlToPass) // hopefully this allows it to use input for the city name 
+  .end((err, res) => {
+    if(err){
+      console.log(err);
+      dataRetreivedSuccessfully = false;
+      console.log("an error has occurred");
+    }
+    else {
+      weatherData = res.body;
+      console.log("weatherData variable set to: " + JSON.stringify(weatherData));
+      dataRetreivedSuccessfully = true;
+      console.log("dataRetreivedSuccessfully: " + dataRetreivedSuccessfully);
+    }
+  });
+};
+function returnValSetter() {
+  console.log("returnValSetter function has been run.");
+  if(dataRetreivedSuccessfully){
+    returnVal = weatherData;
+    console.log("returnVal should be set to weatherData");
+  }
+  if(!dataRetreivedSuccessfully){
+    returnVal = "an error has occurred";
+    console.log("returnVal should be set to predefined error message");
+  }
+};
 
 /* // this is the code that requests the weather data
 request
