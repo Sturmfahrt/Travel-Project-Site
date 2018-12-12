64b0bb46f2963105f5e192bcb85c65c32c0a11d9
Ethan <32519710+Magikro@users.noreply.github.com>
2018-12-12 13:28:47 -0500

    cleaned up

--- Senior Project FV node.js/api/controllers/yelpController.js
+++ Senior Project FV node.js/api/controllers/yelpController.js
@@ -1,93 +1,13 @@
 var term;
 var loca;
 var cost;
-var final;
-var doneGettingAnswer = true;
 const apiKey = 'o-Jyuwf5wA67Pnsnz6SbYsXaq5C7hPP1IODAJxoLrMVseBzSNbv1ANN9B5Jz1OKltC3xJA5G6tJotSOwrLfK4otUXMr26DGCRsQz7f-YspBvUu-j_NsWTCkJQ4TsW3Yx';
-const yelp = require('yelp-fusion');
-const client = yelp.client(apiKey);
-const readline = require('readline-sync');
 var rp = require('request-promise');
 
 exports.get_info = function(req, res) {
     term = req.query.term;
     loca = req.query.location;
     cost = req.query.cost;
-
-    //getAnswer(term, loca, cost); 
-
-    function getAnswer(term, loca, cost) {
-
-        if(cost != null) {
-          console.log("Checking price...");
-          switch(cost) {
-            case "$":
-            cost = 1;
-            break;
-        
-            case "$$":
-            cost = 2;
-            break;
-        
-            case "$$$":
-            cost = 3;
-            break;
-        
-            case "$$$$":
-            cost = 4;
-            break;
-        
-            default:
-            cost = 0;
-          }
-        
-        }
-        console.log("Setting search request...");
-          var searchRequest;
-        
-        if(cost != null) {
-          console.log("Search request with price");
-          searchRequest = { // search terms
-            term: term,
-            location: loca,
-            sort_by: 'rating',
-            price: cost
-          };
-        } else {
-          console.log("Search request without price");
-          searchRequest = { // search terms
-            term: term,
-            location: loca,
-            sort_by: 'rating'
-          };
-        }
-        console.log("Calling the function");
-      
-        return doTheShit(loca, term, searchRequest);
-    }
-      
-    function doTheShit(loca, term, searchRequest) {
-      
-        client.search(searchRequest).then(response => {
-      
-          console.log("Here are the best businesses for '" + term + "' around '"+loca+"'");
-        
-          for(var k = 0; k <= 10; k++) {
-              console.log(response.jsonBody.businesses[k].name + " in " + response.jsonBody.businesses[k].location.city + ", " + response.jsonBody.businesses[k].location.state );
-              final = {
-                  name: response.jsonBody.businesses[k].name,
-                  locationCity: response.jsonBody.businesses[k].location.city,
-                  locationState: response.jsonBody.businesses[k].location.state
-              }
-          }
-        })
-        .catch(e => {
-          console.log(e);
-        });
-        console.log('final is: ');
-        console.log(JSON.stringify(final));
-        res.send(JSON.stringify(final));
-    }
 
     var options = {
       method: 'GET',
