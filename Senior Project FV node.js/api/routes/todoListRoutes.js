6784d79d3ed5de9b941eb3532795a43dff54c1a9
Ethan <32519710+Magikro@users.noreply.github.com>
2018-12-12 13:27:19 -0500

    YELP API DONE!!!!
    celebrate the completion of the yelp api being integrated into our api

--- Senior Project FV node.js/api/routes/todoListRoutes.js
+++ Senior Project FV node.js/api/routes/todoListRoutes.js
@@ -1,6 +1,6 @@
-'use strict';
 module.exports = function(app) {
   var todoList = require('../controllers/todoListController');
+  var yelp = require('../controllers/yelpController');
 
   // todoList Routes
   app.route('/tasks')
@@ -11,4 +11,7 @@ module.exports = function(app) {
     .get(todoList.read_a_task)
     .put(todoList.update_a_task)
     .delete(todoList.delete_a_task);
+
+  app.route('/yelp')
+    .get(yelp.get_info);
 };
